b127992a7f8b7e72513355b07f2b8677eaeb9f40
natannapiorkowski <natan.napiorkowski@gmail.com>
2025-02-15 12:56:55 +0100

    Changed the way folders are added to matlab path

--- RUN.m
+++ RUN.m
@@ -2,9 +2,11 @@
 clear all; close all; clc
 currentPath=mfilename('fullpath');
 currentPath=currentPath(1:strfind(currentPath, mfilename)-1);
-addpath(genpath(currentPath)); cd(currentPath)
+% addpath(genpath(currentPath)); cd(currentPath)
+addpath(currentPath);
+addpath(fullfile(currentPath, 'functions'));
 eeglab
 close all
 
 
-run('MAIN_GUI.mlapp')   
+run('MAIN_GUI.mlapp')
